8984211bfae686bc2564e023e63b4af09a283b2f
dineshch9 <dineshch489@gmail.com>
2024-11-15 23:27:49 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import Input from './Input'
 import { UserButton } from '@clerk/clerk-react'
-import Card from './card'
+import Card from './Card'
 import Button from './Button'
 
 
