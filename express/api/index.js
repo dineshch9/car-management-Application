e74d22ff79e9486632a8472e25c5d24747ab34de
dineshch9 <dineshch489@gmail.com>
2024-11-16 09:12:56 +0530

    express

---
+++ express/api/index.js
@@ -0,0 +1,22 @@
+
+
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/',(req,res)=>res.send("express on versel"));
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => console.log('Server ready on port 3001.'));
+
+module.exports = app;
